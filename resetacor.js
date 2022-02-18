f284eefc948c3366c2b5893b2e13ad785ab66047
taeltico <rafaelwvs16@gmaail.com>
2022-02-18 20:48:42 -0300

    javascript

---
+++ resetacor.js
@@ -0,0 +1,8 @@
+let corSite = 'azul';
+function resetaCor(){
+    corSite = cor;
+}
+console.log(corsite);
+resetaCor("vermelho");
+console.log(corSite) 
+
---
+++ scripts.js
@@ -0,0 +1,17 @@
+ let name = 'Rafael'; // string literal
+ let idade = 25; // number literal
+ let  estaAprovado = true; //boolean
+ let sobrenome = undefined ; //undefined
+ let corSelecionada = null ; // redefinir um valor
+ let sexo = undefined;
+
+ let pessoa = {
+     nome:'Rafael'
+     idade: 25,
+     estAprovado: true,
+     sobrenome: 'de Sá'
+     sexo = 'Masculino'
+
+ } ;
+
+ console.log(pessoa);
